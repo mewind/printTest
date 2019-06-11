676839d6f5abf402016ca2cf63573b48bbec775c
mewind <ngsn-wp14@student.tarc.edu.my>
2019-06-11 10:22:11 +0800

    first commit

---
+++ PrintDocs/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PrintDocs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(48, 367);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(162, 50);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(626, 376);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(162, 50);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.FullName});
+            this.dataGridView1.Location = new System.Drawing.Point(25, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 349);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // FileName
+            // 
+            this.FileName.HeaderText = "FileName";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            // 
+            // FullName
+            // 
+            this.FullName.HeaderText = "FullName";
+            this.FullName.Name = "FullName";
+            this.FullName.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ PrintDocs/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintDocs
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        public class PrintDoc
+        {
+            public string fileName;
+            public string fullName;
+
+            public PrintDoc()
+            {
+                fileName = "";
+                fullName = "";
+            }
+
+        }
+
+        string[] allfiles;
+        BindingList<PrintDoc> fileList = new BindingList<PrintDoc>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            //DialogResult dr = openFileDialog1.ShowDialog();
+
+            //string[] s = openFileDialog1.FileName.Split('.');
+
+            //if (dr.ToString() == "OK")
+
+            //{
+
+            //    if (s.Length > 1)
+
+            //        if (s[1] == "doc" || s[1] == "docx" || s[1] == "jpg")
+
+            //            txtFileName.Text = openFileDialog1.FileName;
+
+            //        else
+
+            //            MessageBox.Show("Please select doc,docx,jpeg file !!");
+
+            //}
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+
+                string printFileName = row.Cells[0].Value.ToString();
+                string printFile = row.Cells[1].Value.ToString();
+                //Using below code we can print any document
+
+
+                ProcessStartInfo info = new ProcessStartInfo(printFile.Trim());
+
+                info.Verb = "Print";
+
+                info.CreateNoWindow = true;
+
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process.Start(info);
+
+
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
+
+                bool success = checkPrintSuccess(printFile);
+                if (success) moveFiles(printFileName, printFile);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            allfiles = Directory.GetFiles(@"Z:\TEST\staffs\Nini\letters\", "*.*", SearchOption.AllDirectories);
+            bindingSource1 = new BindingSource();
+
+            foreach (var file in allfiles)
+            {
+                FileInfo info = new FileInfo(file);
+                // Do something with the Folder or just add them to a list via nameoflist.add();
+                PrintDoc pq = new PrintDoc();
+                pq.fileName = info.Name;
+                pq.fullName = info.FullName;
+
+                fileList.Add(pq);
+                addPrintQueueToList(pq);
+
+            }
+
+
+        }
+
+        private void addPrintQueueToList(PrintDoc pq)
+        {
+            dataGridView1.Rows.Add();
+            DataGridViewRow row = dataGridView1.Rows[dataGridView1.Rows.Count - 1];
+
+            row.Cells[FileName.Index].Value = pq.fileName;
+            row.Cells[FullName.Index].Value = pq.fullName;
+        }
+
+
+        private void moveFiles(string printDoc, string printFilePath)
+        {
+            //foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            //{
+
+            //string fileName = row.Cells[0].Value.ToString();
+            //string sourceFile = @"" + row.Cells[1].Value.ToString();
+            string sourceFile = @"" + printFilePath;
+            string targetPath = @"Z:\TEST\staffs\Nini\letters\SUDAH PRINT\";
+
+            string destFile = Path.Combine(targetPath, printDoc);
+
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            File.Move(sourceFile, destFile);
+
+            //}
+
+            MessageBox.Show("done");
+
+        }
+
+
+        private bool checkPrintSuccess(string docName)
+        {
+
+            bool success = false;
+            PrintServer myPrintServer = new PrintServer();
+
+            // List the print server's queues
+            PrintQueueCollection myPrintQueues = myPrintServer.GetPrintQueues();
+            String printQueueNames = "My Print Queues:\n\n";
+            string printJob = "JOBS:\n\n";
+
+
+            foreach (PrintQueue pq in myPrintQueues)
+            {
+                printQueueNames += "\t" + pq.Name + "\n";
+
+                //HP LaserJet Pro MFP M127fw
+
+                if (pq.Name == "HP LaserJet Pro MFP M127fw")
+                {
+                    try
+                    {
+                        PrintJobInfoCollection jobs = pq.GetPrintJobInfoCollection();
+
+
+                        if (jobs != null)
+                        {
+
+                            foreach (PrintSystemJobInfo job in jobs)
+                            {
+                                printJob += "\t" + job.Name + "\n";
+
+                                if (job.Name == docName && (((job.JobStatus & PrintJobStatus.Completed) == PrintJobStatus.Completed) || ((job.JobStatus & PrintJobStatus.Printed) == PrintJobStatus.Printed)))
+                                {
+                                    success = true;
+                                }
+                                else
+                                    success = false;
+                            }// end for each print job 
+                        }
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        success = true;
+
+                    }
+
+                    break;
+                }
+            }
+            Console.WriteLine(printQueueNames);
+
+            MessageBox.Show(printJob);
+            Console.WriteLine("\nPress Return to continue.");
+            Console.ReadLine();
+
+            return success;
+        }
+
+        // Check for possible trouble states of a print job using the flags of the JobStatus property 
+        internal static void SpotTroubleUsingJobAttributes(PrintSystemJobInfo theJob)
+        {
+            if ((theJob.JobStatus & PrintJobStatus.Blocked) == PrintJobStatus.Blocked)
+            {
+                Console.WriteLine("The job is blocked.");
+            }
+            if (((theJob.JobStatus & PrintJobStatus.Completed) == PrintJobStatus.Completed)
+                ||
+                ((theJob.JobStatus & PrintJobStatus.Printed) == PrintJobStatus.Printed))
+            {
+                Console.WriteLine("The job has finished. Have user recheck all output bins and be sure the correct printer is being checked.");
+            }
+            if (((theJob.JobStatus & PrintJobStatus.Deleted) == PrintJobStatus.Deleted)
+                ||
+                ((theJob.JobStatus & PrintJobStatus.Deleting) == PrintJobStatus.Deleting))
+            {
+                Console.WriteLine("The user or someone with administration rights to the queue has deleted the job. It must be resubmitted.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Error) == PrintJobStatus.Error)
+            {
+                Console.WriteLine("The job has errored.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Offline) == PrintJobStatus.Offline)
+            {
+                Console.WriteLine("The printer is offline. Have user put it online with printer front panel.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.PaperOut) == PrintJobStatus.PaperOut)
+            {
+                Console.WriteLine("The printer is out of paper of the size required by the job. Have user add paper.");
+            }
+
+            if (((theJob.JobStatus & PrintJobStatus.Paused) == PrintJobStatus.Paused)
+                ||
+                ((theJob.HostingPrintQueue.QueueStatus & PrintQueueStatus.Paused) == PrintQueueStatus.Paused))
+            {
+                //HandlePausedJob(theJob);
+                //HandlePausedJob is defined in the complete example.
+            }
+
+            if ((theJob.JobStatus & PrintJobStatus.Printing) == PrintJobStatus.Printing)
+            {
+                Console.WriteLine("The job is printing now.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Spooling) == PrintJobStatus.Spooling)
+            {
+                Console.WriteLine("The job is spooling now.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.UserIntervention) == PrintJobStatus.UserIntervention)
+            {
+                Console.WriteLine("The printer needs human intervention.");
+            }
+
+        }
+
+
+
+    }
+
+
+}
+
